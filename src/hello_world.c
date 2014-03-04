077447b821cd49b608af254f86fcae5dfb3049e5
Quique Fdez Guerra <quique@ckgrafico.com>
2014-03-04 03:50:47 -0800

    Clear spaces
    clean code please

--- src/hello_world.c
+++ src/hello_world.c
@@ -12,27 +12,27 @@ Layer *rootLayer;
 *
 */
 void init(void) {
-  
+
 	// Make a window and get pointer
 	window = window_create();
-  // Get root layer
-  rootLayer = window_get_root_layer(window);
-  // Create example layer
-	textLayer = text_layer_create(GRect(0, 0, 144, 154));   
-	
+	// Get root layer
+	rootLayer = window_get_root_layer(window);
+	// Create example layer
+	textLayer = text_layer_create(GRect(0, 0, 144, 154));
+
 	// Set text in the layer
 	text_layer_set_text(textLayer, "Hellow World");
-  // Set font type to the layer
+	// Set font type to the layer
 	text_layer_set_font(textLayer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  // Set aligment
+	// Set aligment
 	text_layer_set_text_alignment(textLayer, GTextAlignmentCenter);
-	 
+
 	// Add the text layer to the window
 	layer_add_child(rootLayer, text_layer_get_layer(textLayer));
 
 	// Push the window
 	window_stack_push(window, true);
-	
+
 	// App Logging!
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
 }
@@ -43,9 +43,10 @@ void init(void) {
 *
 */
 void deinit(void) {
+
 	// Destroy the text layer
 	text_layer_destroy(textLayer);
-	
+
 	// Destroy the window
 	window_destroy(window);
 }
@@ -57,8 +58,8 @@ void deinit(void) {
 */
 int main(void) {
 	init();
-  // Block in init if we are in the app
-	app_event_loop(); 
-  // Remove rubbish
+	// Block in init if we are in the app
+	app_event_loop();
+	// Remove rubbish
 	deinit();
 }
